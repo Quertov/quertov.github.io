fdfad298226afaeee769074c7cfb78a93c3769cc
lucas <smoooki44@gmail.com>
2025-02-25 09:15:09 +0200

    feat: not found page when not existing locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,8 @@ import {getMessages} from 'next-intl/server';
 import { ReduxProvider } from "@/components/utils/ReduxProvider";
 import { ClientProvider } from "@/components/utils/ClientProvider";
 import { Header } from '@/components/Header';
+import { routing } from '@/i18n/routing';
+import { notFound } from 'next/navigation';
 
 interface LocaleLayoutProps {
   children: React.ReactNode;
@@ -11,7 +13,9 @@ interface LocaleLayoutProps {
 
 export default async function LocaleLayout({ children, params }: LocaleLayoutProps) {
   const { locale } = await params;
-
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
   const messages = await getMessages({ locale });
  
   return (
